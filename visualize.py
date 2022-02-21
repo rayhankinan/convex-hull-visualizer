06bce1ffff75115708228727fd3b495aa59e6758
rayhankinan <rayhankinan@gmail.com>
2022-02-22 03:24:14 +0700

    Menambahkan fungsionalitas

--- visualize.py
+++ visualize.py
@@ -1,17 +1,51 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 from sklearn import datasets
+from scipy.spatial import ConvexHull
 
-# Constants
-COLORS = ["blue", "orange", "green", "red", "purple", "brown", "pink", "gray", "olive", "cyan"]
+# BATASAN: SEMUA DATA MASUKKAN HARUS DALAM NUMERIK
+# BATASAN: DATA TARGET PADA PEMBACAAN CSV HARUS BINARY (AKAN DIUBAH DI MASA DEPAN)
 
+# Class Function
+class Color():
+    COLORS = ["blue", "orange", "green", "red", "purple", "brown", "pink", "gray", "olive", "cyan"]
+    i = 0
+
+    def get_color():
+        new_color = Color.COLORS[Color.i % len(Color.COLORS)]
+        Color.i += 1
+
+        return new_color
+
+def plot_convex_hull(df, first_argument, second_argument, target, class_name):
+    plt.figure(figsize=(10, 6))
+    plt.title(f"{first_argument} vs {second_argument}")
+    plt.xlabel(first_argument)
+    plt.ylabel(second_argument)
+
+    for target_value in df[target].unique():
+        bucket = df[df[target] == target_value]
+
+        hull = ConvexHull(bucket[[first_argument, second_argument]].values)
+
+        plt.scatter(bucket[first_argument].values, bucket[second_argument].values, label=class_name[target_value])
+        color = Color.get_color()
+
+        for simplex in hull.simplices:
+            plt.plot(bucket[[first_argument, second_argument]].values[simplex, 0], bucket[[first_argument, second_argument]].values[simplex, 1], color)
+
+    plt.legend()
+    plt.show()
+
+# Main Program
 if __name__ == "__main__":
     my_parser = ArgumentParser(description="Masukkan nama database serta atribut yang ingin divisualisasi")
 
     my_parser.add_argument("path", metavar="path", type=str, help="relative path menuju file database")
     my_parser.add_argument("first_argument", metavar="first_argument", type=str, help="atribut pertama yang ingin dievaluasi")
     my_parser.add_argument("second_argument", metavar="second_argument", type=str, help="atribut kedua yang ingin dievaluasi")
-    my_parser.add_argument("-t", "--target", type=str, help="atribut yang dijadikan target")
+    my_parser.add_argument("-t", "--target", type=str, help="atribut yang dijadikan target", default="")
 
     args = my_parser.parse_args()
 
@@ -20,21 +54,43 @@ if __name__ == "__main__":
         df = pd.DataFrame(data.data, columns=data.feature_names)
         df["Target"] = pd.DataFrame(data.target)
 
-        print(df.head())
+        if args.first_argument not in data.feature_names or args.second_argument not in data.feature_names or args.target != "":
+            print("Argumen tidak valid!")
+            
+        else:
+            plot_convex_hull(df=df, first_argument=args.first_argument, second_argument=args.second_argument, target="Target", class_name=["Iris-Setosa", "Iris-Versicolour", "Iris-Virginica"])
 
     elif args.path == "wine":
         data = datasets.load_wine()
         df = pd.DataFrame(data.data, columns=data.feature_names)
         df["Target"] = pd.DataFrame(data.target)
 
-        print(df.head())
+        if args.first_argument not in data.feature_names or args.second_argument not in data.feature_names or args.target != "":
+            print("Argumen tidak valid!")
+            
+        else:
+            plot_convex_hull(df=df, first_argument=args.first_argument, second_argument=args.second_argument, target="Target", class_name=["class_0", "class_1", "class_2"])
 
     elif args.path == "breast_cancer":
         data = datasets.load_breast_cancer()
         df = pd.DataFrame(data.data, columns=data.feature_names)
         df["Target"] = pd.DataFrame(data.target)
 
-        print(df.head())
+        if args.first_argument not in data.feature_names or args.second_argument not in data.feature_names or args.target != "":
+            print("Argumen tidak valid!")
+            
+        else:
+            plot_convex_hull(df=df, first_argument=args.first_argument, second_argument=args.second_argument, target="Target", class_name=["WDBC-Malignant", "WDBC-Benign"])
 
     else:
-        pass
+        try:
+            df = pd.read_csv(args.path)
+
+            if args.first_argument not in df.columns or args.second_argument not in df.columns or args.target not in df.columns:
+                print("Argumen tidak valid!")
+            
+            else:
+                plot_convex_hull(df=df, first_argument=args.first_argument, second_argument=args.second_argument, target=args.target, class_name=["fail", "success"]) # BUG: class_name dibaca dari args saja
+
+        except:
+            print(f"Database dengan relative path {args.path} tidak ada!")

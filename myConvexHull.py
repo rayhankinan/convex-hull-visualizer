bd23070d1caf88f2357f0a2934b57073294a731a
rayhankinan <rayhankinan@gmail.com>
2022-02-22 03:45:26 +0700

    Inisialisasi library myConvexHull

---
+++ myConvexHull.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class ConvexHull():
+    def __init__(self, points):
+        self.points = points
+
+    def divide_and_conquer(self):
+        return np.ndarray()
+
+    def is_intersecting(self, other):
+        return False
